af74e206d76fc35ff084023716e7a1256b15e500
Skylark <li_xuelong@126.com>
2017-12-28 19:05:53 +0800

    remove bilibili  magicasakura

--- app/src/main/java/online/himakeit/skylark/AppContext.java
+++ app/src/main/java/online/himakeit/skylark/AppContext.java
@@ -1,14 +1,10 @@
 package online.himakeit.skylark;
 
 import android.app.Application;
-import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Handler;
-import android.support.annotation.ColorInt;
-import android.support.annotation.ColorRes;
 
-import com.bilibili.magicasakura.utils.ThemeUtils;
 import com.facebook.stetho.Stetho;
 import com.litesuits.orm.LiteOrm;
 import com.readystatesoftware.chuck.ChuckInterceptor;
@@ -34,7 +30,6 @@ import okhttp3.Response;
 import online.himakeit.skylark.model.kuaichuan.FileInfo;
 import online.himakeit.skylark.util.LogUtils;
 import online.himakeit.skylark.util.NetUtils;
-import online.himakeit.skylark.util.ThemeHelper;
 import online.himakeit.skylark.util.Toasts;
 
 /**
@@ -44,7 +39,7 @@ import online.himakeit.skylark.util.Toasts;
  * <p>
  * Description:
  */
-public class AppContext extends Application implements ThemeUtils.switchColor {
+public class AppContext extends Application{
 
     private static final String TAG = "AppContext";
     /**
@@ -77,7 +72,6 @@ public class AppContext extends Application implements ThemeUtils.switchColor {
     public void onCreate() {
         super.onCreate();
         mAppContext = this;
-        ThemeUtils.setSwitchColor(this);
         mHandler = new Handler();
 
         initXUtils3();
@@ -444,77 +438,4 @@ public class AppContext extends Application implements ThemeUtils.switchColor {
 
     //==========================================================================
     //==========================================================================
-
-    @Override
-    public int replaceColorById(Context context, @ColorRes int colorId) {
-        if (ThemeHelper.isDefaultTheme(context)) {
-            return context.getResources().getColor(colorId);
-        }
-        String theme = getTheme(context);
-        if (theme != null) {
-            colorId = getThemeColorId(context, colorId, theme);
-        }
-        return context.getResources().getColor(colorId);
-    }
-
-    @Override
-    public int replaceColor(Context context, @ColorInt int originColor) {
-        if (ThemeHelper.isDefaultTheme(context)) {
-            return originColor;
-        }
-        String theme = getTheme(context);
-        int colorId = -1;
-
-        if (theme != null) {
-            colorId = getThemeColor(context, originColor, theme);
-        }
-        return colorId != -1 ? getResources().getColor(colorId) : originColor;
-    }
-
-    private String getTheme(Context context) {
-        if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_STORM) {
-            return "blue";
-        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_HOPE) {
-            return "purple";
-        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_WOOD) {
-            return "green";
-        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_LIGHT) {
-            return "green_light";
-        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_THUNDER) {
-            return "yellow";
-        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_SAND) {
-            return "orange";
-        } else if (ThemeHelper.getTheme(context) == ThemeHelper.CARD_FIREY) {
-            return "red";
-        }
-        return null;
-    }
-
-    private
-    @ColorRes
-    int getThemeColorId(Context context, int colorId, String theme) {
-        switch (colorId) {
-            case R.color.theme_color_primary:
-                return context.getResources().getIdentifier(theme, "color", getPackageName());
-            case R.color.theme_color_primary_dark:
-                return context.getResources().getIdentifier(theme + "_dark", "color", getPackageName());
-            case R.color.theme_color_primary_trans:
-                return context.getResources().getIdentifier(theme + "_trans", "color", getPackageName());
-        }
-        return colorId;
-    }
-
-    private
-    @ColorRes
-    int getThemeColor(Context context, int color, String theme) {
-        switch (color) {
-            case 0xfffb7299:
-                return context.getResources().getIdentifier(theme, "color", getPackageName());
-            case 0xffb85671:
-                return context.getResources().getIdentifier(theme + "_dark", "color", getPackageName());
-            case 0x99f0486c:
-                return context.getResources().getIdentifier(theme + "_trans", "color", getPackageName());
-        }
-        return -1;
-    }
 }
